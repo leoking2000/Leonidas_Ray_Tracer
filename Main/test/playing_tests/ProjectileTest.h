bec974c54a24e35c5d48c56c876963d0f4c70c4d
leoking <Leoking470@gmail.com>
2021-09-05 17:31:48 +0300

    ProjectileTest creates ProjectileTest.PPM

--- Main/test/playing_tests/ProjectileTest.h
+++ Main/test/playing_tests/ProjectileTest.h
@@ -7,15 +7,15 @@
 
 void ProjectileTest()
 {
-	const LRT::vec3 gravity(  0.0f, -0.1f, 0.0f);
+	const LRT::vec3 gravity(  0.0f, -0.2f, 0.0f);
 	const LRT::vec3    wind( -0.01f,  0.0f, 0.0f);
 
 	LRT::vec3 proj_pos(1.0f, 1.0f, 0.0f);
 
-	LRT::vec3 proj_vel(1.0f, 1.0f, 0.0f);
+	LRT::vec3 proj_vel(1.0f, 1.8f, 0.0f);
 	proj_vel.normalize();
 
-	const float initial_speed = 1.5f;
+	const float initial_speed = 11.25f;
 
 	proj_vel *= initial_speed; // speed
 
@@ -31,10 +31,20 @@ void ProjectileTest()
 	const int MaxNumberOfTicks = 500;
 	int numberOfTicks = 0;
 
+	LRT::Canvas cav(900, 550);
+
 	while (proj_pos.y >= 0.0f && numberOfTicks <= MaxNumberOfTicks)
 	{
 		proj_pos = proj_pos + proj_vel;
 		proj_vel = proj_vel + gravity + wind;
+
+
+		uint32_t c_x = (uint32_t)proj_pos.x;
+		uint32_t c_y = cav.GetHeight() - (uint32_t)(proj_pos.y);
+
+		if(c_x >= 0 && c_x < cav.GetWidth() && c_y >= 0 && c_y < cav.GetHeight())
+			cav.SetPixel(c_x, c_y, LRT::Colors::green);
+
 		numberOfTicks++;
 	}
 
@@ -45,6 +55,7 @@ void ProjectileTest()
 	std::cout << "Proj Pos: [" << proj_pos.x << ", " << proj_pos.y << ", " << proj_pos.z << "]\n";
 	std::cout << "Proj Vel: [" << proj_vel.x << ", " << proj_vel.y << ", " << proj_vel.z << "]\n";
 
-
+	cav.SaveToFile("Output/ProjectileTest.PPM");
+	std::cout << "File Output/ProjectileTest.PPM Saved!!!\n";
 
 }
